1dc8d5450b8fc28ca8cdaf4d3839ace9bc819e99
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2023-10-16 00:17:06 +0500

    cart screen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  static const String id = "Cart_Screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/widgets/receipe_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RecipeCard extends StatelessWidget {
+  const RecipeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
