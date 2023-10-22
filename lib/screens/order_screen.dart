2d6356d6edd1030d405fc7ac90a5385e748ca5e9
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2023-10-22 18:45:03 +0500

    Order Screen UI implemented

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:homenom/constants/constants.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
@@ -10,19 +11,51 @@ class OrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            toolbarHeight: 100,
-            flexibleSpace: ImageFiltered(
-              imageFilter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-              child: Image(
-                image: AssetImage("assets/temporary/food_background.jpg"),
-                fit: BoxFit.cover,
+      appBar: AppBar(
+        backgroundColor: kAppBackgroundColor,
+        title: Text("Shop Name here"),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Hero(
+                tag: "RecipePic",
+                child: Image(
+                  image: AssetImage("assets/temporary/food_background.jpg"),
+                  fit: BoxFit.contain,
+                )),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Recipe Name here",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    "This is the textual description of the recipe",
+                  ),
+                  MaterialButton(
+                    onPressed: (){},
+                    child: Text("Add"),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("Price: 350 Rs"),
+                MaterialButton(onPressed: (){}, child: Text("Add to Cart"),)
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
